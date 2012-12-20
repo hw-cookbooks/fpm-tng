edd55747b9b0777333cfafb91f2abf91aa06089f
Chris Roberts <chrisroberts.code@gmail.com>
2012-12-20 10:24:34 -0800

    Update path discovery

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,5 @@ default[:fpm_tng][:install][:gems] = %w(fpm)
 default[:fpm_tng][:install][:packages] = []
 default[:fpm_tng][:build_dir] = '/opt/fpm-build'
 default[:fpm_tng][:package_dir] = '/opt/fpm-pkgs'
-default[:fpm_tng][:exec] = File.join(RbConfig::CONFIG['bindir'], 'fpm')
+default[:fpm_tng][:exec] = File.join(node.languages.ruby.bin_dir, 'fpm')
+default[:fpm_tng][:gem] = File.join(node.languages.ruby.bin_dir, 'gem')
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,21 @@
+begin
+  resources(:ohai => 'ruby')
+  ruby_block 'Reset ruby defaults' do
+    block do
+      node.default[:fpm_tng][:exec] = File.join(node.languages.ruby.bin_dir, 'fpm')
+      node.default[:fpm_tng][:gem] = File.join(node.languages.ruby.bin_dir, 'gem')
+    end
+    action :nothing
+    subscribes 'ohai[ruby]', :immediately
+  end
+rescue Chef::Exceptions::ResourceNotFound
+  Chef::Log.warn 'No ohai ruby reload found.'
+end
+
 node[:fpm_tng][:install][:gems].each do |fpm_gem|
-  gem_package fpm_gem
+  gem_package fpm_gem do
+    gem_binary node[:fpm_tng][:gem]
+  end
 end
 
 node[:fpm_tng][:install][:packages].each do |fpm_package|

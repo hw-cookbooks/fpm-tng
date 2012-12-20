c0e58749dc9882766806304ad4a045a9a4a38fdb
Chris Roberts <chrisroberts.code@gmail.com>
2012-12-20 11:13:46 -0800

    Set gem directly via ohai

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,4 +3,4 @@ default[:fpm_tng][:install][:packages] = []
 default[:fpm_tng][:build_dir] = '/opt/fpm-build'
 default[:fpm_tng][:package_dir] = '/opt/fpm-pkgs'
 default[:fpm_tng][:exec] = File.join(node.languages.ruby.bin_dir, 'fpm')
-default[:fpm_tng][:gem] = File.join(node.languages.ruby.bin_dir, 'gem')
+default[:fpm_tng][:gem] = node.languages.ruby.gem_bin

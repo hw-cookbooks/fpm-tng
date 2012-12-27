2667ea079f1c9d77c38c950078ce2d237c29f33f
Chris Roberts <chrisroberts.code@gmail.com>
2012-12-27 12:14:39 -0800

    Bump version

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## v0.1.1
+* Allow custom package naming within fpm_tng_package LWRP
+
+## v0.1.0
+* Initial release
--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'support@hw-ops.com'
 description 'FPM for Chef'
 license 'Apache 2.0'
 
-version '0.1.0'
+version '0.1.1'
 
 recommends 'reprepro'

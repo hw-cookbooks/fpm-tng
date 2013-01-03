b369b34f60a0b5975688af6efd9a1dacba6b5fc7
Chris Roberts <chrisroberts.code@gmail.com>
2013-01-02 20:41:05 -0800

    Bump patch to 0.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## v0.1.2
+* Add missing action on resource subscription
+
 ## v0.1.1
 * Allow custom package naming within fpm_tng_package LWRP
 
--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'support@hw-ops.com'
 description 'FPM for Chef'
 license 'Apache 2.0'
 
-version '0.1.1'
+version '0.1.2'
 
 recommends 'reprepro'

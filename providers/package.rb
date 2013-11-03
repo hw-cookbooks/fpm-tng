fd2696eaca6c558e8b2f327592304216bf3366b9
Chris Roberts <chrisroberts.code@gmail.com>
2013-11-03 06:27:56 -0800

    Add helper to unbundle environment

--- providers/package.rb
+++ providers/package.rb
@@ -1,10 +1,12 @@
+use_inline_resources if self.respond_to?(:use_inline_resources)
+
 def load_current_resource
   new_resource.version '1.0' unless new_resource.version
   new_resource.package_name new_resource.name unless new_resource.package_name
   unless(new_resource.package)
     new_resource.package ::File.join(node[:fpm_tng][:package_dir], "#{new_resource.name}-#{new_resource.version}.#{new_resource.output_type}")
   end
-  new_resource.workdir node[:fpm_tng][:build_dir] unless new_resource.workdir 
+  new_resource.workdir node[:fpm_tng][:build_dir] unless new_resource.workdir
   new_resource.creates new_resource.package unless new_resource.creates
   if(node[:fpm_tng][:bundle][:enable])
     node.set[:fpm_tng][:exec] = ::File.join(node[:fpm_tng][:bundle][:directory], 'bin/fpm')
@@ -51,21 +53,26 @@ action :create do
         fpm << "--no-#{bool.gsub('_', '-')}"
       end
     end
-    
+
     fpm << "#{Array(new_resource.input_args).join(' ')}"
-    
-    e = execute "Build package - #{new_resource.name}!" do
-      command fpm.join(' ')
-      creates new_resource.package
+
+    unbundle do
+      execute "Build package - #{new_resource.name}!" do
+        command fpm.join(' ')
+        creates new_resource.package
+        cwd '/tmp'
+      end
     end
   end
+
   if(new_resource.reprepro)
-    r = reprepro_deb new_resource.name do
+    reprepro_deb new_resource.name do
       package new_resource.package
     end
   end
+
   if(new_resource.repository)
-    r = repository_package new_resource.package do
+    repository_package new_resource.package do
       repository new_resource.repository
     end
   end
@@ -73,8 +80,25 @@ end
 
 # TODO: Remove from reprepro?
 action :delete do
-  if(::File.exists?(new_resource.package))
-    ::File.delete(new_resource.package)
-    new_resource.updated_by_last_action(true)
+  file new_resource.package do
+    action :delete
+  end
+end
+
+# Break out of the bundle if in one (generally a development issue)
+def unbundle
+  if(defined?(Bundler))
+    Chef::Log.warn "Unbundling and running directly because bundler is a jerk"
+    e_r = yield
+    e_r.action :nothing
+    Bundler.with_clean_env do
+      unless(system(e_r.command))
+        raise Mixlib::ShellOut::ShellCommandFailed.new("Unbundled fpm packaging failed")
+      else
+        new_resource.updated_by_last_action(true)
+      end
+    end
+  else
+    yield
   end
 end
